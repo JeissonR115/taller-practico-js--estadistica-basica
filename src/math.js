88113ebb8f30ea71753fb2938107eeaef8fd4180
JeissonR115 <jeissonfeliper@gmail.com>
2023-08-16 08:06:53 -0500

    add: class RegularPolygon and class Triangle

--- src/math.js
+++ src/math.js
@@ -1,59 +1,72 @@
-const degreesToRadians = (degrees) => degrees * Math.PI / 180;
-
 class RegularPolygon {
-    constructor(numSides, sideLength, unit = "cm") {
-        this.sideLength = sideLength;
+    constructor(numSides, sideLength, unit = "m") {
         this.numSides = numSides;
+        this.sideLength = sideLength;
         this.unit = unit;
-
-        this.angle = 360 / (2 * numSides);
-        this.perimeter = this.calculatePerimeter(sideLength, numSides);
-        this.apothem = this.calculateApothem(sideLength, this.angle);
-        this.area = this.calculateArea(this.perimeter, this.apothem);
+        this.perimeter = this.calculatePerimeter();
+        this.area = this.calculateArea();
     }
 
-    calculateApothem = (sideLength, angle) => sideLength / (2 * Math.tan(degreesToRadians(angle)));
-    calculatePerimeter = (sideLength, numSides) => sideLength * numSides;
-    calculateArea = (perimeter, apothem) => perimeter * apothem / 2;
+    calculatePerimeter = () => this.sideLength * this.numSides;
+    calculateArea = () => (this.numSides * this.sideLength ** 2) / (4 * Math.tan(Math.PI / this.numSides));
 
-    printInfo = (numDecimals, unit = this.unit) => console.log(
+    showInfoPolygon = (numDecimals = 2) =>
         `
-Number of sides: ${this.numSides.toFixed(numDecimals)}
-Side length: ${this.sideLength.toFixed(numDecimals)}${unit}
-Apothem: ${this.apothem.toFixed(numDecimals)}${unit}
-Perimeter: ${this.perimeter.toFixed(numDecimals)}${unit}
-Area: ${this.area.toFixed(numDecimals)}${unit}^2
-`
-    )
+        ***Regular Polygon***
+        Number of sides: ${this.numSides.toFixed(numDecimals)}
+        Side length: ${this.sideLength.toFixed(numDecimals)}${this.unit}
+        Perimeter: ${this.perimeter.toFixed(numDecimals)}${this.unit}
+        Area: ${this.area.toFixed(numDecimals)}${this.unit}^2
+    `;
+
+    static calculatePerimeter = (sideLength, numSides) => sideLength * numSides;
+    static calculateArea = (sideLength, numSides) => (numSides * sideLength ** 2) / (4 * Math.tan(Math.PI / numSides));
 }
-class triangle {
-    constructor(side1, side2, side3,unit) {
-        this.sides = [side1, side2, side3]
-        this.side1 = side1;
-        this.side2 = side2;
-        this.side3 = side3;
+class Triangle {
+    constructor(side1, side2, side3, unit = 'm') {
+        this.sides = [side1, side2, side3];
+        this.numSides = this.sides.length;
         this.unit = unit;
-        
-        this.perimeter = this.calculatePerimeter(this.sides)
-        this.semiPerimeter = this.perimeter/2;
-        this.area = this.calculateArea(this.sides,this.semiPerimeter);
+        this.perimeter = this.calculatePerimeter()
+        this.area = this.calculateArea();
     }
-    calculatePerimeter = (sides ) => sides[0]+sides[1]+sides[2]
-    calculateArea = (sides , semiPerimeter)=> Math.sqrt((semiPerimeter-sides[0])*(semiPerimeter-sides[1])*(semiPerimeter-sides[2])*semiPerimeter , 2)
-    printInfo = (numDecimals, unit = this.unit) => console.log(
-`
-Number of sides: ${this.sides.length}
-Sides length: ${this.sides[0]+ unit} ${this.sides[1]+ unit} ${this.sides[2]+ unit}
-Perimeter: ${this.perimeter.toFixed(numDecimals)}${unit}
-Area: ${this.area.toFixed(numDecimals)}${unit}^2
-`
-    )
+    calculatePerimeter = () => this.sides.reduce((sum, side) => sum + side)
+    calculateArea = () => {
+        const semiPerimeter = this.perimeter / 2;
+        const product = (semiPerimeter - this.sides[0]) * (semiPerimeter - this.sides[1]) * (semiPerimeter - this.sides[2]) * semiPerimeter
+        return Math.sqrt(product, 2);
+    }
+    showInfoTriangle = (numDecimals = 2) => `
+        ***Triangle***
+        Number of sides: ${this.numSides.toFixed(numDecimals)}
+        Side length: ${this.sides.map(side => `${side.toFixed(numDecimals)}${this.unit}`)}
+        Perimeter: ${this.perimeter.toFixed(numDecimals)}${this.unit}
+        Area: ${this.area.toFixed(numDecimals)}${this.unit}^2
+    `
+    static calculatePerimeter = (side1, side2, side3) => {
+        const sides = [side1, side2, side3]
+        return sides.reduce((sum, side) => sum + side);
+    }
+    static calculateArea = (side1, side2, side3) => {
+        const sides = [side1, side2, side3]
+        const perimeter = sides.reduce((sum, side) => sum + side);
+        const semiPerimeter = perimeter / 2;
+        const product = sides.reduce((product, side) => product * (semiPerimeter - side), 1);
+        return Math.sqrt(product * semiPerimeter);
+    };
 }
-const t1 = new triangle(2,4,3);
-t1.printInfo(2,'m')
-const square1 = new RegularPolygon(4, 10, 'cm');
-square1.printInfo(1)
-const triangle1 = new RegularPolygon(3, 1, 'cm');
-triangle1.printInfo(2)
-const square2 = new RegularPolygon(4, 8.5, "m")
-square2.printInfo(5)
+
+
+const scaleneTriangle1 = new Triangle(1, 6, 5);
+const square1 = new RegularPolygon(100000, 10, 'cm');
+const equilateralTriangle = new RegularPolygon(3, 1, 'cm');
+const square2 = new RegularPolygon(4, 8.5);
+const asd = new RegularPolygon(5, 525);
+
+console.log(
+    square1.showInfoPolygon(0),
+    scaleneTriangle1.showInfoTriangle(6),
+    square2.showInfoPolygon(3),
+    asd.showInfoPolygon(0),
+    equilateralTriangle.showInfoPolygon(),
+)
