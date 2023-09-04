074297a188365b2d206144e27706473cf0001005
JeissonR115 <jeissonfeliper@gmail.com>
2023-09-04 11:08:37 -0500

    add files: descuentes.html, descuentos.js

--- src/descuentos.js
+++ src/descuentos.js
@@ -1,1 +1,12 @@
 const calculateDiscount = (price, discount) => (price * (100 - discount)) / 100;
+
+const price = document.querySelector('#price__input');
+const percentage = document.querySelector('#percentage__input');
+const button = document.querySelector('.discount-calculator__button')
+const output = document.querySelector('.discount-calculator__output');
+
+const pDiscount = document.createElement('p');
+button.addEventListener('click',()=>
+{
+    pDiscount.textContent = `El descuento producto con el descuento incluido cuesta : ${calculateDiscount(price.value,percentage.value)}$`;
+output.appendChild(pDiscount);})
