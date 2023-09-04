58ee135665d84467d81a40b8d78eff231d7b71ee
JeissonR115 <jeissonfeliper@gmail.com>
2023-09-04 11:56:22 -0500

    add: validateDiscount(),validatePrice()

--- src/descuentos.js
+++ src/descuentos.js
@@ -1,4 +1,9 @@
-const calculateDiscount = (price, discount) => (price * (100 - discount)) / 100;
+const calculateDiscount = (price, percentage) => (price * (100 - percentage)) / 100
+const validateDiscount = (price, percentage) => {
+    const validatedPercentage = ( !isNaN(percentage) && percentage >= 0 && percentage <= 100)
+    const validatedPrice = (!isNaN(price) && price >= 0)
+    return (validatedPrice && validatedPercentage)
+}
 
 const price = document.querySelector('#price__input');
 const percentage = document.querySelector('#percentage__input');
@@ -6,7 +11,12 @@ const button = document.querySelector('.discount-calculator__button')
 const output = document.querySelector('.discount-calculator__output');
 
 const pDiscount = document.createElement('p');
-button.addEventListener('click',()=>
-{
-    pDiscount.textContent = `El descuento producto con el descuento incluido cuesta : ${calculateDiscount(price.value,percentage.value)}$`;
-output.appendChild(pDiscount);})
+button.addEventListener('click',()=>{
+    const priceValue = parseInt(price.value);
+    const percentageValue = parseInt(percentage.value)
+    pDiscount.textContent = validateDiscount(priceValue, percentageValue)
+        ? `El descuento producto con el descuento incluido cuesta : ${calculateDiscount(priceValue, percentageValue)}$`
+        : `Porfavor ingresa un numero valido`
+
+    output.appendChild(pDiscount);
+});
